42c891a062dfd02fba533e742a93312784ba71e3
duyhung <hung321chiengden@gmail.com>
2023-12-04 12:32:47 +0700

    ehhe

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,10 @@
 import express from "express";
-
+import homeController from '../controller/homeController'
 const router = express.Router();
 const initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send('hello router')
-    })
+    router.get('/', homeController.HandleHelloWorld)
+    router.get('/user', homeController.HandleUser)
+    router.post('/users/create-user', homeController.HandleCreateUser)
     return app.use("/", router);
 }
 export default initWebRoutes
--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import express from "express";
 require('dotenv').config();
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
 
 let app = express();
 configViewEngine(app)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 initWebRoutes(app)
 let PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
